75fdb1412542c93d240495e7c2f1242f04c866e2
lobotuerk <jtlorente@ekumenlabs.com>
2020-08-27 15:24:08 -0300

    first test
    Signed-off-by: lobotuerk <jtlorente@ekumenlabs.com>

---
+++ export_plugin.cc
@@ -0,0 +1,43 @@
+#include <ignition/gazebo/EntityComponentManager.hh>
+#include <ignition/gazebo/System.hh>
+#include "ignition/gazebo/components/Visual.hh"
+#include "ignition/gazebo/components/Name.hh"
+#include <ignition/plugin/Register.hh>
+
+using namespace ignition;
+using namespace gazebo;
+using namespace systems;
+
+class MyPlugin
+      : public System,
+        public ISystemPostUpdate
+{
+
+  public: MyPlugin()
+  {
+
+  }
+
+  public: ~MyPlugin() override
+  {
+
+  }
+
+  public: void PostUpdate(const UpdateInfo &_info,
+      const EntityComponentManager &_ecm)
+  {
+    // ignmsg << "ExportPlugin::PostUpdate" << std::endl;
+    _ecm.Each<components::Visual, components::Name>(
+      [&](const ignition::gazebo::Entity &_entity, const components::Visual *,
+          const components::Name *_nameComp) -> bool
+      {
+        std::cout << _nameComp << std::endl;
+      });
+  }
+};
+
+IGNITION_ADD_PLUGIN(MyPlugin,
+                    System,
+                    MyPlugin::ISystemPostUpdate)
+
+IGNITION_ADD_PLUGIN_ALIAS(MyPlugin,"MyPlugin")
